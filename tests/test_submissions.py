d119a2e5d034c1305677a5e115ab13d3069f767a
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs-erlangen.de>
2018-02-16 18:14:38 +0100

    add pytest tests for submissions

--- tests/test_submissions.py
+++ tests/test_submissions.py
@@ -1,6 +1,5 @@
-from pathlib import Path
+from pathlib import Path as P
 import json
-import numpy as np
 from jsonschema import validate
 import os
 import pytest
@@ -15,36 +14,65 @@ with open(schema_path) as schema_file:
     schema = json.load(schema_file)
 
 
-def generate_folders():
-    p = Path('submissions')
-    for x in p.iterdir():
-        if x.is_dir():
-            yield x
+track_list_path = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    'data/mus18_test_tracks.json',
+)
+
+with open(track_list_path) as tracks_file:
+    track_list = json.load(tracks_file)
 
 
-def generate_jsonfiles(x):
-    p = Path(x)
+def submissions():
+    p = P('submissions')
+    for submission_dir in p.iterdir():
+        if submission_dir.is_dir():
+            yield submission_dir, json_tracks(submission_dir)
+
+
+def json_tracks(x):
+    p = P(x)
     if p.exists():
         json_paths = p.glob('**/*.json')
         for json_path in json_paths:
             yield json_path
 
 
-@pytest.fixture(params=generate_folders())
-def folders(request):
+def check_equal(L1, L2):
+    return len(L1) == len(L2) and sorted(L1) == sorted(L2)
+
+
+@pytest.fixture(
+    params=[(k, v) for k, l in submissions() for v in l],
+    ids=['%s-%s' % (k.stem, v.stem) for k, l in submissions() for v in l]
+)
+def submission_tracks(request):
     return request.param
 
 
-# @pytest.fixture(params=generate_jsonfiles(folders))
-# def json_path(request):
-#     return request.param
+@pytest.fixture(
+    params=submissions(),
+    ids=['%s' % k.stem for k, l in submissions()]
+)
+def submission(request):
+    return request.param[0]
 
 
-def test_folder(folders):
-    assert len(list(generate_jsonfiles(folders))) == 50
+@pytest.fixture
+def track(submission_tracks):
+    return submission_tracks[1]
 
 
-# def test_validation(json_path):
-#     with open(json_path) as json_file:
-#         json_string = json.loads(json_file.read())
-#         validate(json_string, schema)
+def test_submission(submission):
+    # verify that submission name is short than 4 characters
+    assert len(submission.stem) <= 4
+    tracks = list(json_tracks(submission))
+    # verify that all tracks were submitted
+    assert len(list(tracks)) == len(track_list)
+    assert check_equal([track.stem for track in tracks], track_list)
+
+
+def test_scores(track):
+    with open(track) as json_file:
+        json_string = json.loads(json_file.read())
+        validate(json_string, schema)
